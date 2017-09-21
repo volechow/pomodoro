e825f4b581dfe26ee7ea44fe7992d53792ec6d23
Victor Olechow <v.olechow@outlook.de>
2017-09-21 17:39:39 +0200

    some changes, timer no longes accelerates when start is double clicked or more

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-function startTimer() {
+function changeStatus(status) {
+  var status = $("#status").text();
+  status == "break" ? restart("session") : restart("break");
+}
 
+function countdown() {
+  // get current minutes and seconds
   var time = $("#time").text().split(":")
-  minutes = parseInt(time[0])
+  minutes = parseInt(time[0]);
   seconds = parseInt(time[1]);
 
+
   if (minutes == 0 && seconds == 0) {
-    var status = $("#status").text();
-    status == "break" ? restart("session") : restart("break");
+    changeStatus();
   } else if (seconds == 0) {
     $("#time").text((minutes-1)+":"+59);
   } else {
@@ -15,56 +20,56 @@ function startTimer() {
 }
 
 function updateDuration(display, value) {
-    var num = Number(display.text());
-    if (num+value >= 0) {
+    var num = parseInt(display.text());
+    if (num+value > 0) {
       display.text(num + value);
     }
 }
 
 function restart(status) {
-  var start;
   $("#status").text(status);
-  if (status == "break") {
-    start = Number($("#break").text())
-  } else {
-    start = Number($("#session").text())
-  }
+  var start = parseInt($("#"+status).text());
+  console.log(status);
   $("#time").text(start + ":00");
 }
 
 $(document).ready(function() {
-  
+
   var timer;
+  var completed_pomodoros = 0;
+  var isRunning = false;
   restart("session");
 
   $("#plus-break").click(function() {
     updateDuration($("#break"), 1);
   });
-  
+
   $("#minus-break").click(function() {
     updateDuration($("#break"), -1);
   });
-  
+
   $("#plus-session").click(function() {
     updateDuration($("#session"), 1);
   });
-  
+
   $("#minus-session").click(function() {
     updateDuration($("#session"), -1);
   });
-  
+
   $("#start").click(function() {
-    timer = setInterval(startTimer, 1000);
-    startTimer();
+    if (!isRunning) {
+      timer = setInterval(countdown, 1000);
+      isRunning = true;
+    }
   });
 
   $("#pause").click(function() {
     clearInterval(timer);
+    isRunning = false;
   });
 
   $("#restart").click(function() {
-    var status = $("#status").text();
-    status == "break" ? restart("session") : restart("break");
+    restart($("#status").text());
   })
 
 });
